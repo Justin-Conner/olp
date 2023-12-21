57481ab08c07d5afb3985b52c4f30a2a2d3c2952
JustinConner <ohmlotusphoenix@gmail.com>
2023-12-21 07:39:00 -0600

    update

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,15 +11,60 @@ const publicPath = path.join(__dirname, 'public');
 // Serve static files from the 'public' directory
 app.use(express.static(publicPath));
 
-// Serve Bootstrap CSS specifically
-//app.use('/assets/dist/css/bootstrap.min.css', express.static(path.join(publicPath, 'css/bootstrap.min.css')));
-
 // Set up a route to handle the root URL
 app.get('/', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+// Parse form data
+app.use(express.urlencoded({ extended: true }));
+
+// Handle form submissions
+app.post('/submit-form', (req, res) => {
+  // Extract form data
+  const formData = req.body;
+
+  // Send email
+  sendEmail(formData)
+    .then(() => {
+      res.redirect('/index.html');
+    })
+    .catch(error => {
+      console.error('Error sending email:', error);
+      res.status(500).send('Internal Server Error');
+    });
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// Function to send email using nodemailer
+function sendEmail(formData) {
+  // Create a nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'ohmlotusphoenix@gmail.com',  // Replace with your Gmail email address
+      pass: 'qppo avrp uulc kvdw'         // Replace with your Gmail password
+    }
+  });
+
+  // Define the email content
+  const mailOptions = {
+    from: 'ohmlotusphoenix@gmail.com',
+    to: 'jconner@ohmlotusphoenix.com',    // Replace with the recipient's email address
+    subject: 'Consultation Form Submission',
+    text: `
+      Business Name: ${formData.username}
+      First Name: ${formData.firstName}
+      Last Name: ${formData.lastName}
+      Email: ${formData.email}
+      Address: ${formData.address}, ${formData.address2 ? formData.address2 + ',' : ''} ${formData.state} ${formData.zip}
+    `
+  };
+
+  // Return a promise for sending the email
+  return transporter.sendMail(mailOptions);
+}
